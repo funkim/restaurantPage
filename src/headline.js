3472037686291762f6fe61d1950ad041710ffde0
funkim <mcking172@skiff.com>
2024-04-08 20:25:05 -0400

    Buttons now change pages when clicked only each individual page needs to be styled

--- src/headline.js
+++ src/headline.js
@@ -1,12 +1,44 @@
-export function Home() {
+
+let headerContainer = '';
+let aboutContainer = '';
+let menuContainer = '';
+
+export function Home() {    
     const contentContainer = document.querySelector("#content");
-    const headerContainer = document.createElement("div")
+    headerContainer = document.createElement("div")
     const restaurantName = document.createElement("h1")
     const headerHeadline = document.createElement("h2")
-    headerContainer.classList.add("header")
+    menuContainer = document.createElement("div")
+    aboutContainer = document.createElement("div")
+    contentContainer.appendChild(menuContainer)
+    contentContainer.appendChild(aboutContainer)
     contentContainer.appendChild(headerContainer)
+    headerContainer.setAttribute('id','Home');
+    menuContainer.setAttribute('id','Menu');
+    aboutContainer.setAttribute('id','About');
     headerContainer.appendChild(restaurantName)
     restaurantName.innerText = "Garbanzo's Burgers";
     headerContainer.appendChild(headerHeadline)
     headerHeadline.innerText = "The Best Burgers in The Beans-ness!"
+}
+
+export function hidePage(buttonId) {
+    [headerContainer,menuContainer,aboutContainer].forEach(container => {
+        if (container.id === buttonId) {
+            container.classList.remove("hide");
+            container.classList.add("grid")
+        } else {
+            container.classList.add("hide");
+        }
+    });
+}
+
+
+export function buttonClick() {
+    const homeButton = document.querySelector("#Home");
+    const menuButton = document.querySelector("#Menu");
+    const aboutButton = document.querySelector("#About") ;
+    homeButton.addEventListener("click", () => hidePage("Home"));
+    menuButton.addEventListener("click", () => hidePage("Menu"));
+    aboutButton.addEventListener("click", () => hidePage("About"));
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-import { Home } from "./headline";
+import { hidePage, buttonClick, Home } from "./headline";
 import style from "./style.css";
 Home();
+buttonClick();
