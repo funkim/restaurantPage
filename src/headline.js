f99bc2963633796d70c9b2d464230aa6d508c24c
funkim <mcking172@skiff.com>
2024-04-10 11:30:33 -0400

    made javascript more streramlined and not as cluttered.

--- src/headline.js
+++ src/headline.js
@@ -1,42 +1,43 @@
 
-let headerContainer = '';
-let aboutContainer = '';
-let menuContainer = '';
+let headerContainer, menuContainer, aboutContainer;
 
-export function Home() {    
-    const contentContainer = document.querySelector("#content");
-    headerContainer = document.createElement("div")
-    const restaurantName = document.createElement("h1")
-    const headerHeadline = document.createElement("h2")
-    menuContainer = document.createElement("div")
-    aboutContainer = document.createElement("div")
-    contentContainer.appendChild(menuContainer)
-    contentContainer.appendChild(aboutContainer)
-    contentContainer.appendChild(headerContainer)
-    headerContainer.setAttribute('id','Home');
-    menuContainer.setAttribute('id','Menu');
-    aboutContainer.setAttribute('id','About');
-    headerContainer.classList.add("home");
-    menuContainer.classList.add("menu");
-    aboutContainer.classList.add("about");
-    menuContainer.classList.add("hide");
-    aboutContainer.classList.add("hide");
-    headerContainer.appendChild(restaurantName)
-    restaurantName.innerText = "Garbanzo's Burgers";
-    headerContainer.appendChild(headerHeadline)
-    headerHeadline.innerText = "The Best Burgers in The Beans-ness!"
-    const burgerQuote = document.createElement("h2")
-    const quoteSource = document.createElement("h4")
-    headerContainer.appendChild(burgerQuote);
-    headerContainer.appendChild(quoteSource);
-    burgerQuote.innerText = (`"This is the best burger I've had all week!"`)
-    quoteSource.innerText = ("- Man we found standing outside the local homeless shelter")
-    burgerQuote.classList.add("hQuote")
-    quoteSource.classList.add("sQuote")
+function makeContainers(name, elementType, parent) {
+    const container = document.createElement(elementType);
+    container.id = name;
+    parent.appendChild(container);
+    return container; 
 }
 
+export function Home() {
+    const contentContainer = document.querySelector("#content");
+
+    headerContainer = makeContainers("headerContainer", "div", contentContainer);
+    aboutContainer = makeContainers("aboutContainer", "div", contentContainer);
+    menuContainer = makeContainers("menuContainer", "div", contentContainer);
+
+    const restaurantNamed = makeContainers("restaurantName", "h1", headerContainer);
+    const headlineNamed = makeContainers("headline", "h2", headerContainer);
+    const quoteNamed = makeContainers("quote", "h2", headerContainer);
+    const quoteSourceNamed = makeContainers("quoteSource", "h4", headerContainer);
+
+
+    headerContainer.setAttribute('id', 'Home');
+    menuContainer.setAttribute('id', 'Menu');
+    aboutContainer.setAttribute('id', 'About');
+
+    headerContainer.classList.add("home");
+    menuContainer.classList.add("menu", "hide"); 
+    aboutContainer.classList.add("about", "hide");
+    quoteNamed.classList.add("hQuote");
+    quoteSourceNamed.classList.add("sQuote");
+
+    restaurantNamed.innerText = "Garbanzo's Burgers";
+    headlineNamed.innerText = "The Best Burgers in The Beans-ness!";
+    quoteNamed.innerText = `"This is the best burger I've had all week!"`;
+    quoteSourceNamed.innerText = "- Man we found standing outside the local homeless shelter";
+}
 export function hidePage(buttonId) {
-    [headerContainer,menuContainer,aboutContainer].forEach(container => {
+    [headerContainer , menuContainer , aboutContainer].forEach(container => {
         if (container.id === buttonId) {
             if (container.classList.contains('hide')) {
                 container.classList.remove("hide");
@@ -54,9 +55,9 @@ export function hidePage(buttonId) {
 
 
 export function buttonClick() {
-    const homeButton = document.querySelector("#Home");
-    const menuButton = document.querySelector("#Menu");
-    const aboutButton = document.querySelector("#About") ;
+    const homeButton = document.querySelector("#HomeButton");
+    const menuButton = document.querySelector("#MenuButton");
+    const aboutButton = document.querySelector("#AboutButton") ;
     homeButton.addEventListener("click", () => hidePage("Home"));
     menuButton.addEventListener("click", () => hidePage("Menu"));
     aboutButton.addEventListener("click", () => hidePage("About"));
